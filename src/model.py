d8adcb14c85b9976dd3e2568b5ca77fbc18fa4d1
Lou <lou.lemarechal1@outlook.fr>
2025-05-08 12:23:33 +0200

    added some method to class and a working client class

--- src/model.py
+++ src/model.py
@@ -10,22 +10,28 @@ class Client():
     def __init__(self,name : str, email : str) -> None:
         self.name = name
         self._email = email # ajouter un property pour afficher une erreur si l’email n’est pas de la forme ____@___.__
-        self.uuid = uuid.uuid4()
+        self.id = uuid.uuid4()
+
+
+    def __str__(self):
+        return f"{{name : {self.name}; email : {self._email}, id : {self.id}}}"
+        
 
     def to_dictionnary(self) -> dict:
         return {
             "name": self.name,
-            "email": self.email,
-            "uuid": str(self.uuid)
+            "email": self._email,
+            "uuid": str(self.id)
         }
 
 class Room():
-    def __init__(self,name : str ,type : str):
+    def __init__(self,name : str ,type : str, client_uuid : str):
         self.name = name
         self.type = type
-        self._reservations : list[Reservation] = []
-
-    def is_available(self,timestamp) -> bool:
+        self._reservations : list[int] = [] # contient les identifiants des réservations attribuer à la salle
+        self.client_id = client_uuid
+        
+    def is_available(self,timestamp:timedelta) -> bool:
         """Return true if the room is available for the timestamp"""
         pass
 
@@ -33,29 +39,34 @@ class Room():
         return {
             "name": self.name,
             "type": self.type,
+            "reservations": self._reservations,
             
         }
 
 
 
 class Reservation():
-    def __init__(self, room : str, timeSlot : timedelta):
+    def __init__(self, id : int ,room : str, timeSlot : timedelta):
+        self.id = id
         self.room = room
         self.timeSlot = timeSlot
     
     def to_dictionnary(self) -> dict:
         return {
+            "id": self.id,
             "name": self.room,
             "timeSlot": self.timeSlot.__str__,
+            
         }
-
     
+
+
 
 class Controller():
     def __init__(self):
-        self._clients : list[Client] = []
-        self._rooms : list[Room] = []
-        self._reservations : list[Reservation] = []
+        self._clients : dict[uuid.UUID,Client] = {}
+        self._rooms : dict[str,Room] = {}
+        self._reservations : dict[uuid.UUID,Reservation] = {}
 
 
     def load_data(self, file ) -> None:
@@ -68,17 +79,17 @@ class Controller():
 
     def save_data(self, file ) -> None:
         """Save the data to a json file"""
-        clients = []
-        rooms = []
-        reservations = []
+        clients: dict[dict] = {}
+        rooms: dict = {}
+        reservations: dict = {}
         for client in self._clients:
-            clients.append(client.to_dictionnary())
+            clients[client.id] = client.to_dictionnary()
 
         for room in self._rooms:
-            rooms.append(room.to_dictionnary())
+            rooms[room.name] = room.to_dictionnary()
             
         for reservation in self._reservations:
-            reservations.append(reservation.to_dictionnary())
+            reservations[reservation.id] = reservation.to_dictionnary()
             
         data = {
             "clients" : clients,
@@ -93,7 +104,7 @@ class Controller():
     def add_client(self, name : str, email : str) -> None :
         """Add a new client to the model"""
         new_client = Client(name,email)
-        self._clients.append(new_client)
+        self._clients[new_client.id] = new_client
     
     def add_rooms(self, name : str, type : str) -> None:
         """Add a new room to the model"""
@@ -103,7 +114,26 @@ class Controller():
         """Show all the available time slot for a room"""
         pass
 
+    def get_client_infos(self,name : str)->str:
+        clients_infos = []
+        for client in self._clients.values():
+            if client.name == name:
+                clients_infos.append(client.__str__())
+        return clients_infos
+
+
+    
+
     
 
 
 
+if __name__ == "__main__":
+
+    client = Client("Mael", "mael@uha.fr")
+    print(client.__str__())
+    print(type(client.id))
+    print(client.to_dictionnary())
+    controller = Controller()
+    controller.add_client("Mael", "mael@uha.fr")
+    print(controller.get_client_infos("Mael"))
