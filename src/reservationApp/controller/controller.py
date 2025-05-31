2788da0c89fb14b06943686d8550e99e67de89c7
Lou <lou.lemarechal1@outlook.fr>
2025-05-31 21:26:33 +0200

    moved view into a folder

--- src/reservationApp/controller/controller.py
+++ src/reservationApp/controller/controller.py
@@ -1,6 +1,6 @@
 from __future__ import annotations # if some class use type defined later 
 from reservationApp.model.model import *
-from reservationApp.view import View
+from reservationApp.model.view.view import View
 
 class Reservation_app_error(Exception):
     """Base class for all exceptions raised by the reservation app"""
